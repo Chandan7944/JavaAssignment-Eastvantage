7634469bda0612901631c5f044603b65dbe8a339
Raj N, Chandan <crajn@deloitte.com>
2022-09-20 18:35:52 +0530

    Added pojo to manage date range request body

---
+++ src/main/java/com/example/demo/model/DateRangePojo.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class DateRangePojo {
+
+    @DateTimeFormat(style = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    @NotNull(message = "DateRangePojo.class: startDate field cannot be blank.")
+    private Date startDate;
+
+    @DateTimeFormat(style = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    @NotNull(message = "DateRangePojo.class: endDate field cannot be blank.")
+    private Date endDate;
+}
